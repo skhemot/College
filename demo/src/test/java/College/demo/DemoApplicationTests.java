681a2c681136a968ec47f303fd5163245133f515
skhemot <sagarkhemot123@gmail.com>
2025-08-07 22:49:42 +0530

    First commit

---
+++ demo/src/test/java/College/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package College.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
