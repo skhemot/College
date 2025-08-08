a860e81a0c81a7d5f761e7f950b58a1565a77130
skhemot <sagarkhemot123@gmail.com>
2025-08-08 15:56:09 +0530

    .s

--- demo/src/main/java/College/demo/DemoApplication.java
+++ demo/src/main/java/College/demo/DemoApplication.java
@@ -2,13 +2,9 @@ package College.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//this is
 @SpringBootApplication
 public class DemoApplication {
-	int i =0;
-	int o=90;
-	int a=12;
-	int ak;
+//
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
