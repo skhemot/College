3b8e1f298ca12ff7f12bedf2ef1ecc237374142f
skhemot <sagarkhemot123@gmail.com>
2025-08-08 11:08:54 +0530

    a=10

--- demo/src/main/java/College/demo/DemoApplication.java
+++ demo/src/main/java/College/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 	int i =0;
 	int o=90;
+	int a=10;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
