c6825d21f4df8d35a48548eebbcf932a1e766074
skhemot <sagarkhemot123@gmail.com>
2025-08-08 11:18:04 +0530

    Update DemoApplication.java

--- demo/src/main/java/College/demo/DemoApplication.java
+++ demo/src/main/java/College/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 	int i =0;
 	int o=90;
-	int a=10;
+	int a=1;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
