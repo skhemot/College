d5e5d4132825a2f3d99530bfdac5ec46d70207c0
skhemot <sagarkhemot123@gmail.com>
2025-08-08 09:44:42 +0530

    FFirstt

--- demo/src/main/java/College/demo/DemoApplication.java
+++ demo/src/main/java/College/demo/DemoApplication.java
@@ -2,10 +2,10 @@ package College.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//this is test
 @SpringBootApplication
 public class DemoApplication {
-
+	int i =90;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
