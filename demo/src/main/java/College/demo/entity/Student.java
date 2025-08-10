2884f0e6f588ded17a73a438d48ba401a7e2ec92
skhemot <sagarkhemot123@gmail.com>
2025-08-10 21:02:11 +0530

    entityCreated

---
+++ demo/src/main/java/College/demo/entity/Student.java
@@ -0,0 +1,72 @@
+package College.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+public class Student {@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;   // Primary key
+
+    private String name;
+    private int age;
+    private String surname;
+    private int marks;
+    private String result;
+
+    // Constructors
+    public Student() {}
+
+    public Student(String name, int age,String surname,String result,int marks) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters & setters
+    public Long getId() {
+        return id;
+    }
+    public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public int getMarks() {
+		return marks;
+	}
+
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+	}}
