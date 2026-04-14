7c627dc7877bbe1665d97760d3598b0d3f971743
skhemot <sagarkhemot123@gmail.com>
2026-04-14 20:04:30 +0530

    Fix formatting issue in import statement,

--- Student/src/main/java/StudentDiary/demo/controller/StudentController.java
+++ Student/src/main/java/StudentDiary/demo/controller/StudentController.java
@@ -1,6 +1,6 @@
 package StudentDiary.demo.controller;
 
-import java.util.List;
+import java.util.List ;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
