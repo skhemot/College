37aaf8f0401a3ddf562c5a7d08012887f03f77cb
skhemot <sagarkhemot123@gmail.com>
2026-07-10 20:42:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react
+import React from "
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./components/HomePage";
